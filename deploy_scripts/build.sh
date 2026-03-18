8a43e47d284c0cecc3782e4021ddcbd4e2a2a44a
White-Green <43771790+White-Green@users.noreply.github.com>
2026-03-18 20:23:03 +0900

    Update deploy scripts: add MOVE_HTML_TO_RAW argument and conditional logic

--- deploy_scripts/build.sh
+++ deploy_scripts/build.sh
@@ -9,6 +9,15 @@ USER_DATA_PATH="$2"
 BUILD_DATA_PATH="$3"
 SITE_URL="$4"
 PUBLIC_KEY_PATH="$5"
+MOVE_HTML_TO_RAW="$6"
+
+case "$MOVE_HTML_TO_RAW" in
+  true|false) ;;
+  *)
+    echo "[fblog_system] ERROR: MOVE_HTML_TO_RAW must be true or false" >&2
+    exit 1
+    ;;
+esac
 
 HOST_NAME=$(node -e 'const i=process.argv[1]; console.log(new URL(/^https?:\/\//.test(i) ? i : `https://${i}`).hostname);' "$SITE_URL")
 
@@ -19,13 +28,15 @@ cp -r "$ARTICLE_DATA_PATH"/. contents/articles/
 cp -r "$USER_DATA_PATH"/. contents/users/
 cp -r "$BUILD_DATA_PATH"/. public/
 
-for dir in articles users; do
-  mkdir -p "public/raw__/${dir}/html" "public/${dir}"
-  find "public/${dir}" -mindepth 1 -maxdepth 1 ! -name index.html -exec mv -t "public/raw__/${dir}/html" {} + || true
-  cd "public/raw__/${dir}/html"
-  find . -type f -name index.html -print0 | while IFS= read -r -d '' file; do mv "$file" "${file%/*}.html"; done
-  cd - >/dev/null
-done
+if [ "$MOVE_HTML_TO_RAW" = "true" ]; then
+  for dir in articles users; do
+    mkdir -p "public/raw__/${dir}/html" "public/${dir}"
+    find "public/${dir}" -mindepth 1 -maxdepth 1 ! -name index.html -exec mv -t "public/raw__/${dir}/html" {} + || true
+    cd "public/raw__/${dir}/html"
+    find . -type f -name index.html -print0 | while IFS= read -r -d '' file; do mv "$file" "${file%/*}.html"; done
+    cd - >/dev/null
+  done
+fi
 
 pnpm install
 PUBLIC_KEY_FILE="$PUBLIC_KEY_PATH" SITE_URL="https://${HOST_NAME}" pnpm run build
